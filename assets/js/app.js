75b751590065525db96c46009b15ab99297ffeb6
Le Tai <nguyenletai2000@gmail.com>
2022-01-20 21:42:26 +0700

    tai update UI

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,96 +1,130 @@
-'use strict';
+function checkLogin() {
+    var tk = document.getElementById("signin-email").value;
+    var mk = document.getElementById("signin-password").value;
 
-/* ===== Enable Bootstrap Popover (on element  ====== */
+    if (tk=="" || mk=="") {
+        alert('Chưa nhập Username hoặc Password')
+    }
 
-var popoverTriggerList = [].slice.call(document.querySelectorAll('[data-toggle="popover"]'))
-var popoverList = popoverTriggerList.map(function (popoverTriggerEl) {
-  return new bootstrap.Popover(popoverTriggerEl)
+    var url = `https://amesbe.herokuapp.com/api/general/login/${tk}/${mk}`
+    console.log(url)
+    fetch(url,{
+        method:"POST",
+        headers:{
+            'Content-Type': 'application/json',
+            // 'Authorization': sessionStorage.getItem("Token")
+        }
+    }).then(function(reponse){
+        if(reponse.status !=200){
+            alert('Nhập sai Username hoặc Password')
+        }
+        else {
+            return reponse.json()
+        }
+    })
+    .then(function(json){xulyLogin(json)})
+}
+
+function xulyLogin(json){
+     var tk1 = document.getElementById("signin-email").value;
+
+    window.sessionStorage.setItem("Token",json.Token)
+    console.log(typeof(tk1))
+    let result = tk1.substring(0, 2);
+    if(result == "GV"){
+        window.location.replace("./thong_tin_ca_nhan.html")
+    }
+	 if(result == "QL"){
+        window.location.replace("./index_quanly.html")
+    }
+	if(result == "ad"){
+        window.location.replace("./index_admin.html")
+    }
+}
+
+
+function lay_thong_tin_gv(){
+    var url = `https://amesbe.herokuapp.com/api/general/info`
+    console.log(url)
+    fetch(url,{
+        method:"GET",
+        headers:{
+            'Content-Type': 'application/json',
+            'Authorization': sessionStorage.getItem("Token")
+        }
+    }).then((reponse)=>{
+           if(reponse.ok){
+			   reponse.json().then((json)=>{
+				console.log(json)
+				 
+
+				   hienthi_ttgv(json)
+	
+				
+			
+			   })
+		   }
+		   
+		   
+    }).catch((error)=>{
+     console.log(object)
 })
-
-/* ==== Enable Bootstrap Alert ====== */
-var alertList = document.querySelectorAll('.alert')
-alertList.forEach(function (alert) {
-  new bootstrap.Alert(alert)
-});
-
-
-/* ===== Responsive Sidepanel ====== */
-const sidePanelToggler = document.getElementById('sidepanel-toggler'); 
-const sidePanel = document.getElementById('app-sidepanel');  
-const sidePanelDrop = document.getElementById('sidepanel-drop'); 
-const sidePanelClose = document.getElementById('sidepanel-close'); 
-
-window.addEventListener('load', function(){
-	responsiveSidePanel(); 
-});
-
-window.addEventListener('resize', function(){
-	responsiveSidePanel(); 
-});
-
-
-function responsiveSidePanel() {
-    let w = window.innerWidth;
-	if(w >= 1200) {
-	    // if larger 
-	    //console.log('larger');
-		sidePanel.classList.remove('sidepanel-hidden');
-		sidePanel.classList.add('sidepanel-visible');
-		
-	} else {
-	    // if smaller
-	    //console.log('smaller');
-	    sidePanel.classList.remove('sidepanel-visible');
-		sidePanel.classList.add('sidepanel-hidden');
-	}
-};
-
-sidePanelToggler.addEventListener('click', () => {
-	if (sidePanel.classList.contains('sidepanel-visible')) {
-		console.log('visible');
-		sidePanel.classList.remove('sidepanel-visible');
-		sidePanel.classList.add('sidepanel-hidden');
-		
-	} else {
-		console.log('hidden');
-		sidePanel.classList.remove('sidepanel-hidden');
-		sidePanel.classList.add('sidepanel-visible');
-	}
-});
+}
 
 
 
-sidePanelClose.addEventListener('click', (e) => {
-	e.preventDefault();
-	sidePanelToggler.click();
-});
 
-sidePanelDrop.addEventListener('click', (e) => {
-	sidePanelToggler.click();
-});
+function hienthi_ttgv(json){
+	document.getElementById("maso-gv").value = json.TK;
+    document.getElementById("hoten-gv").value = json.HoTen;
+    document.getElementById("cccd-gv").value = json.MaCanCuoc;
+    document.getElementById("ngaysinh-gv").value = json.NgaySinh;
+    if (json.GioiTinh == false) {
+        document.getElementById("gioitinh-gv").value = "Nam";
+    } else {
+        document.getElementById("gioitinh-gv").value = "Nữ";
+    }
+    document.getElementById("dienthoai-gv").value = json.SDT;
+    document.getElementById("diachi-gv").value = json.DiaChi;
 
+    window.sessionStorage.setItem("MK",json.MK);
+}
 
+function doi_mat_khau_gv(){
+    var old_pass_gv = document.getElementById("old_pass_gv").value;
+    var new_pass_gv = document.getElementById("new_pass_gv").value;
+    var re_new_pass_gv = document.getElementById("re_new_pass_gv").value;
 
-/* ====== Mobile search ======= */
-const searchMobileTrigger = document.querySelector('.search-mobile-trigger');
-const searchBox = document.querySelector('.app-search-box');
+    var url = `https://amesbe.herokuapp.com/api/general/info`
 
-searchMobileTrigger.addEventListener('click', () => {
+    if (old_pass_gv != sessionStorage.getItem("MK")){
+        alert('Nhập sai mật khẩu hiện tại.')
+    } else {
+        if (new_pass_gv != re_new_pass_gv){
+            alert('Nhập lại mật khẩu mới không đúng với mật khẩu mới.')
+        } else {
 
-	searchBox.classList.toggle('is-visible');
-	
-	let searchMobileTriggerIcon = document.querySelector('.search-mobile-trigger-icon');
-	
-	if(searchMobileTriggerIcon.classList.contains('fa-search')) {
-		searchMobileTriggerIcon.classList.remove('fa-search');
-		searchMobileTriggerIcon.classList.add('fa-times');
-	} else {
-		searchMobileTriggerIcon.classList.remove('fa-times');
-		searchMobileTriggerIcon.classList.add('fa-search');
-	}
-	
-		
-	
-});
+        }
+    }
+    
+
+    // var url = `https://amesbe.herokuapp.com/api/general/info`
+    // console.log(url)
+    // fetch(url,{
+    //     method:"POST",
+    //     headers:{
+    //         'Content-Type': 'application/json',
+    //         'Authorization': sessionStorage.getItem("Token")
+    //     }
+    // }).then(function(reponse){
+    //         return reponse.json()
+    // })
+    // .then(function(json){dmk_gv(json)})
+}
+
+function dmk_gv(json){
+
+}
+
 
 
